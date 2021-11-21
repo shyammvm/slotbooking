846e7cfb98806b6ce2d59f6d50ccf53ce64985d5
keval <keval291997813@gmail.com>
2021-11-21 12:07:45 +0530

    user adding part completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   #for user
+  root "users#admin_user"
+  resources :users
+  #get "/users", to: "users#index"
+  # get "/articles", to: "articles#index"
 end
