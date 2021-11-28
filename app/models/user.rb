a87b3e53006cdca61a1d98df2bed3ece6ee3e614
shyammvm <shyam.vmmohan@gmail.com>
2021-11-28 17:19:58 +0530

    misc

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-    has_many :books
+   
   has_secure_password
+  has_many :books
 
   validates :email, presence: true, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: 'must be a valid email address' }
   validates :name, presence: true
