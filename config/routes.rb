1e1e9750b2518d9f8040240a6366d88ca59fac7d
keval <keval291997813@gmail.com>
2021-11-28 17:16:14 +0530

    creating booking is working

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   #for user
   #root"books#index"
-  # root"books#new"
+  #root"books#new"
   resources :books
   #root "users#admin_user"
   #resources :users
