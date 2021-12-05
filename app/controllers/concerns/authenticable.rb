8f5df6f0ed351aaf63e1702f4d74334e438b050b
shyammvm <shyam.vmmohan@gmail.com>
2021-12-05 22:58:29 +0530

    removed unwanted functions

--- app/controllers/concerns/authenticable.rb
+++ app/controllers/concerns/authenticable.rb
@@ -6,12 +6,5 @@ module Authenticable
 
     @current_user = User.find(session[:user_id])
   end
-
-  def current_user
-    return @current_user if @current_user
-  end
-
-  def check_login
-    head :forbidden unless @current_user
-  end
+  
 end
